c8fdcdce59fe42d78bd2df0b61d6cb0d78da6cc5
Amanda Reilly <amandamichellereilly@gmail.com>
2018-04-21 12:52:32 -0700

    Update gulpfile to handle nested template structure.

--- gulpfile.js
+++ gulpfile.js
@@ -20,80 +20,90 @@ const runSequence = require('run-sequence');
 gulp.task('default', ['watch']);
 
 gulp.task('build-css', function() {
-  return gulp.src('source/scss/**/*.scss')
-    .pipe(sourcemaps.init())
-      .pipe(sass())
-      .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-      }))
-      //only minify if run with '--type production'
-      .pipe(gutil.env.type === 'production' ? cssnano() : gutil.noop())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('public/assets/css'))
-    .pipe(livereload());
+    return gulp.src('source/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        //only minify if run with '--type production'
+        .pipe(gutil.env.type === 'production' ? cssnano() : gutil.noop())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/assets/css'))
+        .pipe(livereload());
 });
 
 gulp.task('compile-templates', function() {
-  //process and register partials
-  //assume that all partials start with an underscore
-  const partials = gulp.src('source/templates/**/_*.hbs')
-    .pipe(handlebars())
-    .pipe(wrap('Handlebars.registerPartial(<%= processPartialName(file.relative) %>, Handlebars.template(<%= contents %>));', {}, {
-      imports: {
-        processPartialName: function(fileName) {
-          // Strip the extension and underscore
-          // Escape the output with JSON.stringify
-          return JSON.stringify(fileName.slice(1, -3));
-        }
-      }
-    }));
-  const templates = gulp.src('source/templates/**/[^_]*.hbs')
-  .pipe(handlebars())
-  .pipe(wrap('Handlebars.template(<%= contents %>)'))
-  .pipe(declare({
-    namespace: 'templates',
-    noRedeclare: true // Avoid duplicate declarations
-  }));
+    //process and register partials
+    //assume that all partials start with an underscore
+    const partials = gulp.src('source/templates/**/_*.hbs')
+        .pipe(handlebars())
+        .pipe(wrap('Handlebars.registerPartial(<%= processPartialName(file.relative) %>, Handlebars.template(<%= contents %>));', {}, {
+            imports: {
+                processPartialName: function(fileName) {
+                    // Strip the extension and underscore
+                    // Escape the output with JSON.stringify
+                    const fileArr = fileName.split('\\');
+                    let name = '';
+                    for (let i = 0; i < fileArr.length; i++) {
+                        if (i !== fileArr.length - 1) {
+                            name += fileArr[i] + '_';
+                        } else {
+                            name += fileArr[i].slice(1, -3);
+                        }
+                    }
+                    return JSON.stringify(name);
 
-  //output both the partials and the templates as js/templates.js
-  return merge(partials, templates)
-    .pipe(concat('templates.js'))
-    .pipe(gulp.dest('source/js/templates'));
+                }
+            }
+        }));
+    const templates = gulp.src('source/templates/**/[^_]*.hbs')
+        .pipe(handlebars())
+        .pipe(wrap('Handlebars.template(<%= contents %>)'))
+        .pipe(declare({
+            namespace: 'templates',
+            noRedeclare: true // Avoid duplicate declarations
+        }));
+
+    //output both the partials and the templates as js/templates.js
+    return merge(partials, templates)
+        .pipe(concat('templates.js'))
+        .pipe(gulp.dest('source/js/templates'));
 
 });
 
 gulp.task('build-js', function() {
-  return gulp.src('source/js/classes/*.js')
-  .pipe(addsrc.append('source/js/data/*.js'))
-  .pipe(addsrc.append('source/js/templates/*.js'))
-  .pipe(addsrc.append('source/js/functions/*.js'))
-  .pipe(addsrc.append('source/js/*.js'))
-    .pipe(sourcemaps.init())
-      .pipe(concat('bundle.js'))
-      //only uglify if run with '--type production'
-      .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('public/assets/js'))
-    .pipe(livereload());
+    return gulp.src('source/js/classes/*.js')
+        .pipe(addsrc.append('source/js/data/*.js'))
+        .pipe(addsrc.append('source/js/templates/*.js'))
+        .pipe(addsrc.append('source/js/functions/*.js'))
+        .pipe(addsrc.append('source/js/*.js'))
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js'))
+        //only uglify if run with '--type production'
+        .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/assets/js'))
+        .pipe(livereload());
 });
 
 gulp.task('copy-html', function() {
-  gulp.src('source/*.html').pipe(gulp.dest('public'))
-  .pipe(livereload());
+    gulp.src('source/*.html').pipe(gulp.dest('public'))
+        .pipe(livereload());
 });
 
 gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch('source/templates/**/*.hbs', ['compile-templates']);
-  gulp.watch('source/*.html', ['copy-html']);
-  gulp.watch('source/scss/**/*.scss', ['build-css']);
-  gulp.watch('source/js/**/*.js', ['build-js']);
+    livereload.listen();
+    gulp.watch('source/templates/**/*.hbs', ['compile-templates']);
+    gulp.watch('source/*.html', ['copy-html']);
+    gulp.watch('source/scss/**/*.scss', ['build-css']);
+    gulp.watch('source/js/**/*.js', ['build-js']);
 });
 
 gulp.task('build', function() {
-  runSequence(
-    ['build-css', 'compile-templates', 'copy-html'],
-    'build-js'
-  );
+    runSequence(
+        ['build-css', 'compile-templates', 'copy-html'],
+        'build-js'
+    );
 });
